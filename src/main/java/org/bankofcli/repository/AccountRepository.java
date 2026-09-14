0958bb3ca73f6d9f95e7f2a5887ebd17e44e3b86
Chandramouli Iyer <iyerchandramouli03@gmail.com>
2026-09-14 08:29:39 -0700

    Add javadoc headers to all interfaces and most implementations

--- src/main/java/org/bankofcli/repository/AccountRepository.java
+++ src/main/java/org/bankofcli/repository/AccountRepository.java
@@ -6,8 +6,35 @@ import java.util.Optional;
 import org.bankofcli.model.Account;
 
 public interface AccountRepository {
+    /**
+     * Creates a new account with the details contained in the account object.
+     * 
+     * @param account The account object containing the details to be saved.
+     * @return Returns the newly created account.
+     */
     Account create(Account account);
+
+    /**
+     * Finds and returns, if exists, an account with the specified account ID. Null otherwise.
+     * 
+     * @param accountId The account ID of the account to be fetched.
+     * @return The Account object if it exists. Null otherwise.
+     */
     Optional<Account> findById(String accountId);
+
+    /**
+     * Returns a boolean indicating if an account with the specified account ID exists.
+     * 
+     * @param accountId The account ID of the account to be searched.
+     * @return True if the account exists. False otherwise.
+     */
     boolean existsById(String accountId);
+
+    /**
+     * Returns the current balance of the account specified by the account ID.
+     * 
+     * @param accountId The account ID of the account for which the balance needs to be fetched.
+     * @return The current balance of the account.
+     */
     BigDecimal getBalance(String accountId);
 }
--- src/main/java/org/bankofcli/repository/sqlite/SQLiteAccountRepository.java
+++ src/main/java/org/bankofcli/repository/sqlite/SQLiteAccountRepository.java
@@ -8,24 +8,52 @@ import org.bankofcli.repository.AccountRepository;
 
 public class SQLiteAccountRepository implements AccountRepository {
 
+    /**
+     * Creates a new account with the details contained in the account object.
+     * 
+     * @param account The account object containing the details to be saved.
+     * @return Returns the newly created account.
+     * @see AccountRepository#create(Account)
+     */
     @Override
     public Account create(Account account) {
         // TODO: Make sure to create a UUID for the Account ID. Otherwise, it won't be accepted into the database.
         throw new UnsupportedOperationException("Unimplemented method 'create'");
     }
 
+    /**
+     * Finds and returns, if exists, an account with the specified account ID. Null otherwise.
+     * 
+     * @param accountId The account ID of the account to be fetched.
+     * @return The Account object if it exists. Null otherwise.
+     * @see AccountRepository#findById(String)
+     */
     @Override
     public Optional<Account> findById(String accountId) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'findById'");
     }
 
+    /**
+     * Returns a boolean indicating if an account with the specified account ID exists.
+     * 
+     * @param accountId The account ID of the account to be searched.
+     * @return True if the account exists. False otherwise.
+     * @see AccountRepository#existsById(String)
+     */
     @Override
     public boolean existsById(String accountId) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'existsById'");
     }
 
+    /**
+     * Returns the current balance of the account specified by the account ID.
+     * 
+     * @param accountId The account ID of the account for which the balance needs to be fetched.
+     * @return The current balance of the account.
+     * @see AccountRepository#getBalance(String)
+     */
     @Override
     public BigDecimal getBalance(String accountId) {
         // TODO Auto-generated method stub
