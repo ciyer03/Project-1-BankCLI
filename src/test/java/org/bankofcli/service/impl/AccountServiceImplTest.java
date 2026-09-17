6c863b954b16b538d8853ba4488b1cbb99c86dc3
gr33npo1s0n <iyerchandramouli03@gmail.com>
2026-09-17 12:43:11 -0700

    Add AccountServiceImpl tests

---
+++ src/test/java/org/bankofcli/service/impl/AccountServiceImplTest.java
@@ -0,0 +1,38 @@
+package org.bankofcli.service.impl;
+
+import java.math.BigDecimal;
+
+import org.bankofcli.exceptions.BankingException;
+import org.bankofcli.model.Account;
+import org.bankofcli.repository.memory.InMemoryBankRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountServiceImplTest {
+    private InMemoryBankRepository repository;
+    private AccountServiceImpl accounts;
+    private TransactionServiceImpl transactions;
+
+    @BeforeEach
+    void setup() {
+        repository = new InMemoryBankRepository();
+        accounts = new AccountServiceImpl(repository);
+        transactions = new TransactionServiceImpl(repository, repository);
+        repository.create(new Account("", "", "Alice1-", 1234));
+    }
+
+    @Test
+    void getBalanceReturnsZeroForNewAccountAndReflectsDeposit() {
+        assertEquals(new BigDecimal("0.00"), accounts.getBalance("Alice1-"));
+        transactions.deposit("Alice1-", new BigDecimal("15.50"));
+        assertEquals(new BigDecimal("15.50"), accounts.getBalance("Alice1-"));
+    }
+
+    @Test
+    void getBalanceRejectsMissingOrBlankAccount() {
+        assertThrows(BankingException.class, () -> accounts.getBalance("Missing1-"));
+        assertThrows(BankingException.class, () -> accounts.getBalance(""));
+        assertThrows(BankingException.class, () -> accounts.getBalance(null));
+    }
+}
